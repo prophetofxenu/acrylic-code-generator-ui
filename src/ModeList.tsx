4d775717bdb36a27b2779bcda1ece2a42bb14e4d
William Harrell <wmharrel@ncsu.edu>
2021-10-20 11:26:48 -0400

    implemented mode list editor

--- src/ModeList.tsx
+++ src/ModeList.tsx
@@ -1,8 +1,9 @@
-import { Mode } from './code-gen/modes';
+import { SolidMode, Mode, CandleMode, MarqueeMode, RainbowMode, SpectrumCycleMode } from './code-gen/modes';
 
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 import styles from './App.module.scss';
+import React, { useRef } from 'react';
 
 
 interface ModeListItemProps { 
@@ -34,14 +35,96 @@ function ModeListItem({ mode, index, isActive, setActive }: ModeListItemProps) {
   )
 }
 
+interface ModeListEditorProps {
+  addMode: (m: string) => void,
+  deleteSelectedMode: () => void
+}
+
+function ModeListEditor({ addMode, deleteSelectedMode }: ModeListEditorProps) {
+
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  return (
+    <div className={styles.modeListEditor}>
+      <label htmlFor="addMode">Add mode:</label>
+      <select name="addMode" className={styles.select} ref={selectRef}>
+        <option value="solid">Solid</option>
+        <option value="candle">Candle</option>
+        <option value="marquee">Marquee</option>
+        <option value="rainbow">Rainbow</option>"
+        <option value="spectrumcycle">Spectrum Cycle</option>
+      </select>
+      <button type="button" className={styles.button}
+        onClick={() => {
+          const val = selectRef.current?.value;
+          if (val === undefined) return;
+          addMode(val);
+        }}
+      >Add</button>
+      <br />
+      <button type="button" className={styles.button}
+        onClick={deleteSelectedMode}
+      >Delete Selected</button>
+    </div>
+  )
+}
+
 interface ModeListProps {
   modes: Mode[],
   setModeList: (modes: Mode[]) => void,
   activeMode: Mode | null,
-  setActiveMode: (mode: Mode) => void
+  setActiveMode: (mode: Mode | null) => void
 }
 
 export default function ModeList({ modes, setModeList, activeMode, setActiveMode }: ModeListProps) {
+
+  const addMode = (m: string) => {
+    const arr = Array.from(modes);
+    let mode: Mode;
+    switch(m) {
+      case "solid":
+        mode = new SolidMode();
+        arr.push(mode);
+        setModeList(arr);
+        setActiveMode(mode);
+        break;
+      case "candle":
+        mode = new CandleMode();
+        arr.push(mode);
+        setModeList(arr);
+        setActiveMode(mode);
+        break;
+      case "marquee":
+        mode = new MarqueeMode();
+        arr.push(mode);
+        setModeList(arr);
+        setActiveMode(mode);
+        break;
+      case "rainbow":
+        mode = new RainbowMode();
+        arr.push(mode);
+        setModeList(arr);
+        setActiveMode(mode);
+        break;
+      case "spectrumcycle":
+        mode = new SpectrumCycleMode();
+        arr.push(mode);
+        setModeList(arr);
+        setActiveMode(mode);
+        break;
+      default:
+        throw Error(`${m} not implemented`);
+    }
+  };
+
+  const deleteSelectedMode = () => {
+    if (activeMode === null) return;
+    const arr = Array.from(modes);
+    const idx = arr.indexOf(activeMode);
+    arr.splice(idx, 1);
+    setModeList(arr);
+    setActiveMode(null);
+  };
 
   const onDragEnd = (result: any) => {
     const { destination, source, draggableId } = result;
@@ -58,6 +141,7 @@ export default function ModeList({ modes, setModeList, activeMode, setActiveMode
   return (
     <div className={styles.modeList}>
       <h2 className={styles.modeListHeading}><b>Modes</b></h2>
+      <ModeListEditor addMode={addMode} deleteSelectedMode={deleteSelectedMode}/>
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="mode-list-droppable">
           {(provided) => (
